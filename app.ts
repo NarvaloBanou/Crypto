aa2cce8dde4c1a8263fe9d99f805d56f1a667cfa
Esteban PAGIS <espagis@hotmail.fr>
2024-05-09 21:44:05 +0200

    Affichage des transactions sur notre serveur local

--- app.ts
+++ app.ts
@@ -3,27 +3,25 @@ const axios = require("axios");
 const app = express();
 const port = 3000;
 
-//test (print static exemple in localhost://3000)
-app.get('/', (req, res) => {
-    res.json("Delta : 24")
-});
-
 // Function retrieving the history of the last 100 transactions of a cryptocurrency pair
 async function getTransactions(cryptoPair) {
     try {
         const response = await axios.get(`https://api.kucoin.com/api/v1/market/histories?symbol=${cryptoPair}`);
-        return response.data.data; // return transaction data
+        return response.data.data; // return transactions data
     } catch (error) {
         console.error('Error while retrieving data', error);
         throw error;
     }
 }
-//using getTransactions to print all data in the console
-getTransactions('BTC-USDT')
-.then((transactions) => {
-    console.log('KuCoin transactions for the pair BTC-USDT :', transactions);
-    // Display all the data in the console
-})
+app.get('/', async (req, res) => {
+
+    // Retrieve transaction data for the given pair (ex: BTC-USDT)
+    const cryptoPair = 'BTC-USDT';
+    const transactions = await getTransactions(cryptoPair);
+
+    // return transactions using JSON
+    res.json(transactions);
+});
 
 app.listen(port, () => {
     console.log(`Server launched on port: ${port}`);
